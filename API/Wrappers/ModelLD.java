b77fea96e930a2c8b67fe6495e62be4ee3d2a67e
Marneus901 <circadian@live.com>
2012-07-26 17:55:43 -0400

    cleanup of code

--- API/Wrappers/ModelLD.java
+++ API/Wrappers/ModelLD.java
@@ -9,6 +9,8 @@
 ********************************************************/
 package com.Marneus.Bot.API.Wrappers;
 
+import com.Marneus.Bot.API.Methods.Calculations;
+import com.Marneus.Bot.API.Methods.Client;
 import com.Marneus.Enviroment.Data;
 import com.Marneus.Enviroment.Hook.ClassHook;
 import com.Marneus.Enviroment.Hook.ClassHook.FieldHook;
@@ -92,5 +94,52 @@ public class ModelLD {
 			}
 		}		
 		return new short[]{};
+	}
+	public int[][] projectVertices(int localX, int localY) {
+		RenderLD renderData = Client.getRenderLD();
+		Viewport render = renderData.getViewport();
+		float[] data = render.getFloats();
+		double locX = (localX+0.5)*512;
+		double locY = (localY+0.5)*512;
+		int numVertices = Math.min(getVerticiesX().length, Math.min(getVerticiesY().length, getVerticiesZ().length));
+		int[][] screen = new int[numVertices][3];
+
+		float xOff = data[12];
+		float yOff = data[13];
+		float zOff = data[15];
+		float xX = data[0];
+		float xY = data[4];
+		float xZ = data[8];
+		float yX = data[1];
+		float yY = data[5];
+		float yZ = data[9];
+		float zX = data[3];
+		float zY = data[7];
+		float zZ = data[11];
+
+		int height = Calculations.tileHeight((int)locX, (int)locY);
+		for (int index = 0; index < numVertices; index++) {
+			int vertexX = (int) (getVerticiesX()[index] + locX);
+			int vertexY = getVerticiesY()[index] + height;
+			int vertexZ = (int) (getVerticiesZ()[index] + locY);
+			
+			float _z = (zOff + (zX * vertexX + zY * vertexY + zZ * vertexZ));
+			float _x = (xOff + (xX * vertexX + xY * vertexY + xZ * vertexZ));
+			float _y = (yOff + (yX * vertexX + yY * vertexY + yZ * vertexZ));
+			
+			float fx = ((float)256.0 + ((float)256.0 * _x) / _z);
+			float fy = ((float)166.0 + ((float)167.0 * _y) / _z);
+			if(fx<520 && fx>0 && fy<390 && fy>50){
+				screen[index][0] = (int)fx;
+				screen[index][1] = (int)fy;
+				screen[index][2] = 1;
+			}
+			else{
+				screen[index][0] = -1;
+				screen[index][1] = -1;
+				screen[index][2] = 0;
+			}
+		}
+		return screen;
 	}
 }
